490de0b74f0815d72d8602614577fb3a6487d9c4
Ahmed Qureshi <ahmedbq1993@gmail.com>
2022-08-16 01:11:41 -0400

    First push

--- HighFive/ViewController.swift
+++ HighFive/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // TODO: Make screen compatible with most iphone sizes somehow
+        // TODO: Maybe try to use bounds to calculate the width 
+        
+        let gridView = GridView(position: CGPoint(x: 0, y: 0), dimension: 5, tilePadding: 3, tileWidth: 60)
+        view.addSubview(gridView)
+        gridView.center.x = view.center.x
+        gridView.center.y = 300
     }
 
 
