18797caf29368ef8ef761bef921175a1b7cd961e
Ahmed Qureshi <ahmedbq1993@gmail.com>
2022-08-17 01:29:31 -0400

    Added TODO

--- HighFive/ViewController.swift
+++ HighFive/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // TODO: Either disallow layout view or implement viewWillTransition(to:with:)
     }
     
     override func viewDidLayoutSubviews() {
